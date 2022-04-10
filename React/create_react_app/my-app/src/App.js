a2bf54288c6283d29a72e224bc40f013e7d54b4a
Ryan Meza <ryanmeza116@gmail.com>
2022-04-10 18:38:56 -0400

    First React

---
+++ React/create_react_app/my-app/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+return (
+  <div className = 'app'>
+    <h1>Hello Dojo!</h1>
+    <h3>Things I need to do:</h3>
+    <ul>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Feed the dog</li>
+    </ul>
+  </div>
+);
+}
+
+export default App;
