135cb64d5887218137ed12f4a7a0b1f902370de2
Ryan Meza <ryanmeza116@gmail.com>
2022-04-16 14:19:20 -0400

    Hook form assignment

---
+++ React/hook_form/src/components/form.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react'; 
+
+// creating variables where user info can be stored and changed : 
+
+const Form = (props) => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setlastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+
+// creating object to hold styling options for results table : 
+
+const formStyle = {
+    textAlign: 'left',
+    width: '450px',
+    margin: 'auto'
+}
+
+const inputStyle = {
+    borderRadius: '5px',
+    backgroundColor: '#f2f2f2',
+    border: '1px solid darkgrey',
+    padding: '0px 10px',
+    margin: '5px'
+}
+
+return (
+    <div>
+        <form style = {{ marginTop:'20px'}}>
+            <div style = {inputStyle}>
+                <label htmlFor = "firstName">First Name: </label>
+                <input 
+                type = "text"
+                name = "firstName"
+                onChange = { (e) => setFirstName (e.target.value)}
+                />
+            </div>
+            <div style = {inputStyle}>
+                <label htmlFor = "lastName">Last Name: </label>
+                <input 
+                type = "text"
+                name = "lastName"
+                onChange = { (e) => setlastName (e.target.value)}
+                />
+            </div>
+            <div style = {inputStyle}>
+                <label htmlFor = "email">Email: </label>
+                <input 
+                type = "text"
+                name = "email"
+                onChange = { (e) => setEmail (e.target.value)}
+                />
+            </div>
+            <div style = {inputStyle}>
+                <label htmlFor = "password">Password : </label>
+                <input 
+                type = "password"
+                name = "password"
+                onChange = { (e) => setPassword (e.target.value)}
+                />
+            </div>
+            <div style = {inputStyle}>
+                <label htmlFor = "confirmPassword">Confirm Password: </label>
+                <input 
+                type = "password"
+                name = "confirmPassword"
+                onChange = { (e) => setConfirmPassword (e.target.value)}
+                />
+            </div>
+        </form>
+
+        <div style = {formStyle}>
+            <h3 style = {{textAlign:'center'}}>Your Form Data</h3>
+            <p>
+                <label>First Name: </label>{ firstName }
+            </p>
+            <p>
+                <label>Last Name: </label> {lastName}
+            </p>
+            <p>
+                <label>Email: </label> {email}
+            </p>
+            <p>
+                <label> Password: </label> {password}
+            </p>
+            <p>
+                <label> Confirm Password: </label> {confirmPassword}
+            </p>
+        </div>
+    </div>
+)
+}
+export default Form;
+
+// Questions for 1 on 1: 
+// 1: How to style? Is styling css or bootstrap? 
+// 2: What is e? How does it work, why + when do we use it? 
+// 3: Why do we use props but never call it? 
