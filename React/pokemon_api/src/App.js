9e31dbd2569a44623b65a2cc88db26bcbca31aaf
Ryan Meza <ryanmeza116@gmail.com>
2022-04-29 17:28:30 -0400

    Pokemon changed to Axios

--- React/pokemon_api/src/App.js
+++ React/pokemon_api/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'; 
 
 function App() {
   // We expect an array of pokemon objects back and so we will
@@ -9,30 +10,23 @@ function App() {
 
   // this will run immediately after the JSX is rendered
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
-      .then(response => {
-
-        return response.json()
-      })
-      .then(response => {
-
-        setPokemon(response.results)
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-    
-
-  }, []);  
+    axios
+    .get('https://pokeapi.co/api/v2/pokemon?limit=807')
+        .then((response) => {
+          console.log(response.data.results);
+          setPokemon(response.data.results);
+        }  )
+        .catch((err) => console.log(err));
+  }, []);
+      
 
   return (
     <div className="App" style={{ width: "200px", margin: "auto" }}>
+      <h1> Axios Pokemon Api </h1>
       <ul>
-        {
-          pokemon.map((pokemonObj, index)=>{
-            return (<li key={index}>{pokemonObj.name}</li>)
-          })
-        }
+        {pokemon.map((pokemon, index) => (
+          <li key = {index}>{pokemon.name}</li>
+        ))}
       </ul>
     </div>
   );
