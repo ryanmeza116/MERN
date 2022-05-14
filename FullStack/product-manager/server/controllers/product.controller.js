fc75d42c07dce60547ed2be9a9de4d284c1463b1
Ryan Meza <ryanmeza116@gmail.com>
2022-05-13 20:18:00 -0400

    removed node mods

--- FullStack/product-manager/server/controllers/product.controller.js
+++ FullStack/product-manager/server/controllers/product.controller.js
@@ -4,8 +4,34 @@ module.exports = {
 
     createProduct: (req,res) => {
         Product.create(req.body)
-            .then((newProduct) => res.json(newProduct))
+            .then((newProduct) => {
+                console.log(newProduct);
+                res.json(newProduct)
+            })
             .catch ((err) => console.log(err));
-    }
+    },
 
+    getAllProducts : (req,res) => {
+        Product.find({})
+        // .then((allProducts) => console.log(allProducts)) res.json(allProducts))
+        // .then((allProducts) => 
+        //     console.log(allProducts), res.json(allProducts))
+        .then(allProducts =>{
+            res.json(allProducts);
+        })
+            // .catch ((err) => 
+            // console.log(err));
+        .catch(err=> {
+            res.status(400).json({message: "Something went wrong in getAllMovies", error : err});
+        });
+    }, 
+
+    getOneProduct : (req,res) => {
+        Product.findOne({_id: req.params.id})
+        .then ((oneProduct) => {
+            console.log(oneProduct);
+            res.json(oneProduct);
+        })
+        .catch ((err) => console.log(err))
+    }
 }
--- FullStack/product-manager/server/routes/product.routes.js
+++ FullStack/product-manager/server/routes/product.routes.js
@@ -1,5 +1,8 @@
+const productController = require('../controllers/product.controller');
 const ProductController = require('../controllers/product.controller');
 
 module.exports = (app) => {
-    app.post('/api/products', ProductController.createProduct)
-}
+    app.post('/api/products', ProductController.createProduct);
+    app.get('/api/products/', ProductController.getAllProducts );
+    app.get('/api/products/:id', productController.getOneProduct);
+};
